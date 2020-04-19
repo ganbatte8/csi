5139c49080d98c5d141cd62f53cd2f8744633d2b
ganbatte8 <vincent.drouot@yahoo.com>
2020-04-19 13:25:15 +0200

    inputs ajouter_patient.php

--- ajouter_patient.php
+++ ajouter_patient.php
@@ -1,17 +1,17 @@
-<form action = "ajouter_patient.php" method = "get">
-    numss:
-    prénom:
-    nom:
-    étatSanté:
-    étatSurveillance:
-    dateNaissance:
-    genre:
-    numTelephone:
-    email:
-    adresse.numero:
-    adresse.rue:
-    adresse.codePostal:
-    adresse.nomVille:
-    departement: 
+<form action = "ajouter_patient.php" method = "post">
+    numss: <input type="text" name="numss">
+    prénom: <input type="text" name="prenom">
+    nom: <input type="text" name="nom">
+    etatSante: <input type="text" name="etatSante">
+    etatSurveillance: <input type="text" name="etatSurveillance">
+    dateNaissance: <input type="text" name="dateNaissance">
+    genre: <input type="text" name="genre">
+    numTelephone: <input type="text" name="numTelephone">
+    email: <input type="text" name="email">
+    adresse.numero: <input type="text" name="adresse.numero">
+    adresse.rue: <input type="text" name="adresse.rue">
+    adresse.codepostal: <input type="text" name="adresse.codepostal">
+    adresse.nomVille: <input type="text"name="adresse.nomville">
+    departement: <input type="select" name="departement">
     <input type="submit">
 </form>
