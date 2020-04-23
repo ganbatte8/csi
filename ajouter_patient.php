64a7d7cbe51560af57b25f52f6d797029b78459a
LECOMPTE Yvan <lecompteyvan@gmail.com>
2020-04-23 17:14:28 +0200

    insertion works

--- ajouter_patient.php
+++ ajouter_patient.php
@@ -1,6 +1,6 @@
 <?php
 
-include('includes/includes_theme/includes_up.php'); 
+include('includes/includes_theme/includes_up.php');
 // initialise $dbconn et $result. Il faudra ensuite inclure includes_down.php
 
 if ($_SESSION['metier'] != 0) {
@@ -9,24 +9,50 @@ if ($_SESSION['metier'] != 0) {
 
 
 $query = "";
+
+echo "<table>";
+foreach ($_POST as $key => $value) {
+    echo "<tr>";
+    echo "<td>";
+    echo $key;
+    echo "</td>";
+    echo "<td>";
+    echo $value;
+    echo "</td>";
+    echo "</tr>";
+}
+echo "</table>";
 if (isset($_POST['sendfeedback'])){
-    //, $_POST['prenom'],$_POST['nom'], $_POST['datenaissance'], $_POST['genre'], $_POST['numtelephone'], $_POST['adressep'], $_POST['email']
+    echo "sendfeedback isset !";
+}
+if (isset($_POST['prenom'])){
+    echo "prenom isset !";
+}
+if (isset($_POST['sendfeedback'],$_POST['prenom'])){
+    echo "both isset !";
+}
+if (isset($_POST['sendfeedback'], $_POST['prenom'],$_POST['nom'], $_POST['datenaissance'], $_POST['genre'], $_POST['numtelephone'], $_POST['adressep'], $_POST['email'])){
+    if ($_POST['genre'] == 'Homme'){
+        $_POST['genre'] = 'H';
+    }
+    elseif ($_POST['genre'] == 'Femme'){
+        $_POST['genre'] = 'F';
+    }
+    elseif ($_POST['genre'] == 'Autre'){
+        $_POST['genre'] = 'A';
+    }
     echo "helloooooooooooooooo";
-    echo $_POST['numss'];
-    echo $_POST['prenom'];
-    echo $_POST['nom'];
-    echo $_POST['datenaissance'];
-    echo $_POST['genre'];
-    echo $_POST['numtelephone'];
-    echo $_POST['adressep'];
-    echo $_POST['email'];
-    $query =  "INSERT INTO patient(numss, prenom, nom, etatsante, etatsurveillance, datenaissance, genre, numtelephone, adressep, email)
-      VALUES ('$_POST[numss]','$_POST[prenom]','$_POST[nom]','aucun symptôme','quarantaine', '$_POST[datenaissance]', '$_POST[genre]', 
-      '$_POST[numtelephone]', '$_POST[adressep]', '$_POST[email]');";
-    // Prépare une requête pour l'exécution
-    $result = pg_prepare($dbconn, "my_query", $query);
-    header('Location: listepatients.php');
-    $result = pg_execute($dbconn, "my_query");
+    $query =  "INSERT INTO patient(numss, prenom, nom, etatsante, etatsurveillance, datenaissance, genre, numtelephone, adressep, email) VALUES ( {$_POST['numss']}, '{$_POST['prenom']}', '{$_POST['nom']}', 'aucun symptôme','quarantaine', '{$_POST['datenaissance']}', '{$_POST['genre']}', {$_POST['numtelephone']}, '{$_POST['adressep']}', '{$_POST['email']}');";
+    
+    // $result = pg_prepare($dbconn, "my_query", $query);
+    // echo "avant header";
+    // $result = pg_execute($dbconn,"my_query");
+    // header('Location: listepatients.php');
+    // echo "apres header";
+    if (pg_query($dbconn, $query)) {
+        header('Location: /listepatients.php');
+      }
+    
 }
 
 
@@ -36,10 +62,10 @@ if (isset($_POST['sendfeedback'])){
 
 
 
-<form>
+<form action="/ajouter_patient.php" method="post">
     <div class="form-group">
-        <label for="inputState">Genre</label>
-        <select id="inputState" class="form-control">
+        <label for="genre">Genre</label>
+        <select id="genre" class="form-control" name="genre">
             <option selected>Choisissez le genre</option>
             <option>Homme</option>
             <option>Femme</option>
@@ -55,26 +81,26 @@ if (isset($_POST['sendfeedback'])){
         <input type="text" class="form-control" id="prenom" name="prenom" placeholder="Jean">
     </div>
     <div class="form-group ">
-        <label for="numeross">Numéro de sécurité social</label>
-        <input type="text" class="form-control" id="numéross" name="numss" placeholder="0123456789123">
+        <label for="numss">Numéro de sécurité social</label>
+        <input type="text" class="form-control" id="numss" name="numss" placeholder="0123456789123">
     </div>
     <div class="form-group ">
-        <label for="numeross">Date de naissance</label>
-        <input type="text" class="form-control" id="numéross" name="datenaissance" placeholder="2020-02-14">
+        <label for="datenaissance">Date de naissance</label>
+        <input type="text" class="form-control" id="datenaissance" name="datenaissance" placeholder="2020-02-14">
     </div>
     <div class="form-group">
         <label for="numtel">Numéro de téléphone</label>
         <input type="text" class="form-control" id="numtel" name="numtelephone" placeholder="0647789856">
     </div>
     <div class="form-group col-md12">
-        <label for="adresse">Adresse</label>
-        <input type="text" class="form-control" id="adresse" name="adressep" placeholder="102 Rue du Verger Nancy">
+        <label for="adressep">Adresse</label>
+        <input type="text" class="form-control" id="adressep" name="adressep" placeholder="102 Rue du Verger Nancy">
     </div>
     <div class="form-group col-md6">
         <label for="email">Adresse mail</label>
         <input type="text" class="form-control" id="email" name="email" placeholder="truc@gmail.com">
     </div>
-    <button type="submit" name= "sendfeedback" class="btn btn-primary">Ajout patient</button>
+    <button type="submit" id="sendfeedback" name="sendfeedback" class="btn btn-primary">Ajout patient</button>
 
 
 </form>
@@ -94,7 +120,7 @@ if (isset($_POST['sendfeedback'])){
 
 <?php
 // free $result et $dbconn definis dans init.php. init.php est inclus dans includes_up.php
-include('includes/includes_theme/includes_down.php'); 
+include('includes/includes_theme/includes_down.php');
 ?>
 
 <!-- Page level plugin JavaScript-->
