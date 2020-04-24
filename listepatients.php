4e9de6034b570d5929494b016aac8d62fbdbeb62
LECOMPTE Yvan <lecompteyvan@gmail.com>
2020-04-24 17:19:56 +0200

    Fin de la galère

--- listepatients.php
+++ listepatients.php
@@ -106,7 +106,6 @@ array_pop($data_hop);
         }).then(function(val) {
             var etat = val
             if (etat == "fièvre et problèmes respiratoires" || etat == "inconscient") {
-                alert("TRUE")
                 var data_hop = JSON.parse(document.getElementById("dom-target").textContent);
                 console.log(data_hop);
                 var data = {};
@@ -116,61 +115,43 @@ array_pop($data_hop);
                 swal({
                     title: 'Dans quel hôpital ?',
                     input: 'select',
-                    inputOptions: {
-                        'aucun symptôme': 'aucun symptôme',
-                        'fièvre': 'fièvre',
-                        'fièvre et problèmes respiratoires': 'fièvre et problèmes respiratoires',
-                        'décédé': 'décédé',
-                        'inconscient': 'inconscient'
-                    },
+                    inputOptions: data,
+                    /* {
+                                  'aucun symptôme': 'aucun symptôme',
+                                  'fièvre': 'fièvre',
+                                  'fièvre et problèmes respiratoires': 'fièvre et problèmes respiratoires',
+                                  'décédé': 'décédé',
+                                  'inconscient': 'inconscient'
+                              },
+                              */
                     showCancelButton: true,
                     confirmButtonColor: '#3085d6',
                     cancelButtonColor: '#d33',
                     confirmButtonText: 'Valider !',
                     cancelButtonText: "Annuler"
-                }).then(function(val) {
-                    var etat = val
-                    if (etat == "fièvre et problèmes respiratoires" || etat == "inconscient") {
-                        var data_hop = JSON.parse(document.getElementById("dom-target").textContent);
-                        console.log(data_hop);
-                        var data = {};
-                        for (var value of data_hop) {
-                            data[value.idhp] = value.nom
-                        }
-                        swal({
-                            title: 'Dans quel hôpital ?',
-                            input: 'select',
-                            inputOptions: data,
-                            showCancelButton: true,
-                            confirmButtonColor: '#3085d6',
-                            cancelButtonColor: '#d33',
-                            confirmButtonText: 'Valider !',
-                            cancelButtonText: "Annuler"
-                        }).then(function(val2) {
-                            post('/listepatients.php', {
-                                idhp: val2,
-                                etat: etat,
-                                numss: numss
-                            })
-                            swal(
-                                'OK !',
-                                'Le patient a bien été modifié',
-                                'success'
-                            )
-                        });
-                    } else {
-                        post('/listepatients.php', {
-                            idhp: -1,
-                            etat: etat,
-                            numss: numss
-                        })
-                        swal(
-                            'KO !',
-                            'Le patient a bien été modifié',
-                            'success'
-                        )
-                    }
+                }).then(function(val2) { // d'ou vient val2 ?
+                    post('/listepatients.php', {
+                        idhp: val2,
+                        etat: etat,
+                        numss: numss
+                    })
+                    swal(
+                        'OK !',
+                        'Le patient a bien été modifié',
+                        'success'
+                    )
+                });
+            } else {
+                post('/listepatients.php', {
+                    idhp: -1,
+                    etat: etat,
+                    numss: numss
                 })
+                swal(
+                    'KO !',
+                    'Le patient a bien été modifié',
+                    'success'
+                )
             }
         })
     }
@@ -184,120 +165,121 @@ array_pop($data_hop);
 
 <div id="content-wrapper">
 
-    <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-10">
+            <h1 class="page-header">
+                Liste des patients
 
-
-
-        </h1>
-    </div>
-    <div class="col-lg-2">
-        <a href="/ajouter_patient.php" style="float: right"><button type="button" class="btn btn-info">Ajouter un patient</button> </a>
-    </div>
-</div>
-
-
-<hr>
-
-<?php
-$query_alert = "SELECT * FROM hopital WHERE idhp = {$_SESSION['idhp']};";
-$result_alert = pg_query($query_alert) or die('Échec de la requête : ' . pg_last_error());
-while ($arr[] = pg_fetch_array($result_alert, NULL, PGSQL_ASSOC));
-array_pop($arr);
-if ($arr[0]['tauxmax'] <= 100 * $arr[0]['placeoccupe'] / $arr[0]['placelibre']) {
-    echo "<p>Attention : Le taux de remplissage de votre hôpital est critique.</p>";
-}
-
-?>
-
-<div class="row" style="width: 1000px; margin: auto;">
-    <center>
-        <form method="POST">
-            <div class="row">
-                <div class="form-check form-check-inline">
-                    <label class="form-check-label" for="all"><input type="radio" name="preselections" value="all" id="all" onClick="this.form.submit();" <?= $checked['all'] ?>>Tous les patients</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <label class="form-check-label" for="dep"><input type="radio" name="preselections" value="dep" id="dep" onClick="this.form.submit();" <?= $checked['dep'] ?>>A contacter (département)</label>
-                </div>
-                <div class="form-check form-check-inline">
-                    <label class="form-check-label" for="hop"><input type="radio" name="preselections" value="hop" id="hop" onClick="this.form.submit();" <?= $checked['hop'] ?>>A contacter (hôpital)</label>
-                </div>
-
-            </div>
-
-        </form>
-    </center>
-
-
-</div>
-
-
-<script>
-    function clearradio() {
-        document.getElementById("all").checked = false;
-        document.getElementById("dep").checked = false;
-        document.getElementById("hop").checked = false;
-    }
-</script>
-
-
-
-<!-- DataTables Example -->
-<div class="card mb-3">
-    <div class="card-header">
-        <i class="fas fa-table"></i>
-        Liste des patients </div>
-    <div class="card-body">
-        <div class="table-responsive">
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                    <tr>
-                        <th>Numéro de sécurité social</th>
-                        <th>Prénom</th>
-                        <th>Nom</th>
-                        <th>État santé</th>
-                        <th>État de surveillance</th>
-                        <th>Date de naissance</th>
-                        <th>Genre</th>
-                        <th>Numéro de téléphone</th>
-                        <th>Adresse</th>
-                        <th>Email</th>
-                        <th>Département</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    if ($data) {
-                        foreach ($data as $d) { ?>
-
-                            <tr>
-                                <td><?= $d['numss'] ?></td>
-                                <td><?= $d['prenom'] ?></td>
-                                <td><?= $d['nom'] ?></td>
-                                <td><?= $d['etatsante'] ?></td>
-                                <td><?= $d['etatsurveillance'] ?></td>
-                                <td><?= $d['datenaissance'] ?></td>
-                                <td><?= $d['genre'] ?></td>
-                                <td><?= $d['numtelephone'] ?></td>
-                                <td><?= $d['adressep'] ?></td>
-                                <td><?= $d['email'] ?></td>
-                                <td><?= $d['iddep'] ?></td>
-                                <td>
-                                    <button onClick="javascript:modifPatient('<?= $d['numss'] ?>')" class="btn btn-primary" title="Modification du patient"> <i class="fas fa-pen-square"></i></i></button>
-                                </td>
-                            </tr>
-
-                    <?php
-                        }
-                    }
-                    ?>
-                </tbody>
-                </tbody>
-            </table>
+            </h1>
+        </div>
+        <div class="col-lg-2">
+            <a href="/ajouter_patient.php" style="float: right"><button type="button" class="btn btn-info">Ajouter un patient</button> </a>
         </div>
     </div>
-</div>
+
+
+    <hr>
+
+    <?php
+    $query_alert = "SELECT * FROM hopital WHERE idhp = {$_SESSION['idhp']};";
+    $result_alert = pg_query($query_alert) or die('Échec de la requête : ' . pg_last_error());
+    while ($arr[] = pg_fetch_array($result_alert, NULL, PGSQL_ASSOC));
+    array_pop($arr);
+    if ($arr[0]['tauxmax'] <= 100 * $arr[0]['placeoccupe'] / $arr[0]['placelibre']) {
+        echo "<p>Attention : Le taux de remplissage de votre hôpital est critique.</p>";
+    }
+
+    ?>
+
+    <div class="row" style="width: 1000px; margin: auto;">
+        <center>
+            <form method="POST">
+                <div class="row">
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label" for="all"><input type="radio" name="preselections" value="all" id="all" onClick="this.form.submit();" <?= $checked['all'] ?>>Tous les patients</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label" for="dep"><input type="radio" name="preselections" value="dep" id="dep" onClick="this.form.submit();" <?= $checked['dep'] ?>>A contacter (département)</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <label class="form-check-label" for="hop"><input type="radio" name="preselections" value="hop" id="hop" onClick="this.form.submit();" <?= $checked['hop'] ?>>A contacter (hôpital)</label>
+                    </div>
+
+                </div>
+
+            </form>
+        </center>
+
+
+    </div>
+
+
+    <script>
+        function clearradio() {
+            document.getElementById("all").checked = false;
+            document.getElementById("dep").checked = false;
+            document.getElementById("hop").checked = false;
+        }
+    </script>
+
+
+
+    <!-- DataTables Example -->
+    <div class="card mb-3">
+        <div class="card-header">
+            <i class="fas fa-table"></i>
+            Liste des patients </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Numéro de sécurité social</th>
+                            <th>Prénom</th>
+                            <th>Nom</th>
+                            <th>État santé</th>
+                            <th>État de surveillance</th>
+                            <th>Date de naissance</th>
+                            <th>Genre</th>
+                            <th>Numéro de téléphone</th>
+                            <th>Adresse</th>
+                            <th>Email</th>
+                            <th>Département</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        if ($data) {
+                            foreach ($data as $d) { ?>
+
+                                <tr>
+                                    <td><?= $d['numss'] ?></td>
+                                    <td><?= $d['prenom'] ?></td>
+                                    <td><?= $d['nom'] ?></td>
+                                    <td><?= $d['etatsante'] ?></td>
+                                    <td><?= $d['etatsurveillance'] ?></td>
+                                    <td><?= $d['datenaissance'] ?></td>
+                                    <td><?= $d['genre'] ?></td>
+                                    <td><?= $d['numtelephone'] ?></td>
+                                    <td><?= $d['adressep'] ?></td>
+                                    <td><?= $d['email'] ?></td>
+                                    <td><?= $d['iddep'] ?></td>
+                                    <td>
+                                        <button onClick="javascript:modifPatient('<?= $d['numss'] ?>')" class="btn btn-primary" title="Modification du patient"> <i class="fas fa-pen-square"></i></i></button>
+                                    </td>
+                                </tr>
+
+                        <?php
+                            }
+                        }
+                        ?>
+                    </tbody>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 
 
 
